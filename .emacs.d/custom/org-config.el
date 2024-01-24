0b44b234e85295ffa6c8b36b99a561f40e4dabf2
Nicholas Hébert <68243838+n-hebert@users.noreply.github.com>
2024-01-24 14:34:25 -0400

    Comment out ob-go and Org Protocol again

--- .emacs.d/custom/org-config.el
+++ .emacs.d/custom/org-config.el
@@ -23,41 +23,41 @@
 (with-eval-after-load 'evil-maps (define-key evil-motion-state-map (kbd "TAB") nil))
 
 ;; ---- Automatically add CREATED property with the timestamp on new headings --- ;;
-(defun my/log-todo-creation-date (&rest ignore)
-  "Log TODO creation time in the property drawer under the key 'CREATED'."
-  (when (not (org-entry-get nil "CREATED"))
-    (org-entry-put nil "CREATED" (format "%s" (format-time-string (org-time-stamp-format t t) (current-time)))
-                   )
-    )
-  )
-(advice-add 'org-insert-todo-heading :after #'my/log-todo-creation-date)
-(advice-add 'org-insert-todo-heading-respect-content :after #'my/log-todo-creation-date)
-(advice-add 'org-insert-heading-respect-content :after #'my/log-todo-creation-date)
-(advice-add 'org-insert-heading-after-current :after #'my/log-todo-creation-date)
-(add-hook 'org-capture-before-finalize-hook #'my/log-todo-creation-date)
+; (defun my/log-todo-creation-date (&rest ignore)
+;   "Log TODO creation time in the property drawer under the key 'CREATED'."
+;   (when (not (org-entry-get nil "CREATED"))
+;     (org-entry-put nil "CREATED" (format "%s" (format-time-string (org-time-stamp-format t t) (current-time)))
+;                    )
+;     )
+;   )
+; (advice-add 'org-insert-todo-heading :after #'my/log-todo-creation-date)
+; (advice-add 'org-insert-todo-heading-respect-content :after #'my/log-todo-creation-date)
+; (advice-add 'org-insert-heading-respect-content :after #'my/log-todo-creation-date)
+; (advice-add 'org-insert-heading-after-current :after #'my/log-todo-creation-date)
+; (add-hook 'org-capture-before-finalize-hook #'my/log-todo-creation-date)
 ;; -- End automatically add CREATED - ;;
 
 ;; ---- Enable golang in OrgMode ---- ;;
-(require 'ob-go)
-(org-babel-do-load-languages
-  'org-babel-load-languages
-  '((go . t)))
+; (require 'ob-go)
+; (org-babel-do-load-languages
+;   'org-babel-load-languages
+;   '((go . t)))
 ;; ------ End golang in OrgMode ----- ;;
 
 ;; ------ Permit org-capture -------- ;;
 ;; https://orgmode.org/worg/org-contrib/org-protocol.html
 ;; https://github.com/sprig/org-capture-extension for firefox
-(load "server")
-(unless (server-running-p) (server-start))
+; (load "server")
+; (unless (server-running-p) (server-start))
 
-(require 'org-protocol)
+; (require 'org-protocol)
 
-(setq org-capture-templates `(
-                              ("p" "Protocol" entry (file+headline ,(concat org-directory "Home.org") "Captured Quotes")
-                               "* %^{Title}\nSource: [[%:link][%:description]]\nCaptured On: %u,\n#+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n")
-                              ("L" "Protocol Link" entry (file+headline ,(concat org-directory "Home.org") "Captured Links")
-                               "* %?[[%:link][%:description]]\nCaptured On: %U")
-                              ))
+; (setq org-capture-templates `(
+;                               ("p" "Protocol" entry (file+headline ,(concat org-directory "Home.org") "Captured Quotes")
+;                                "* %^{Title}\nSource: [[%:link][%:description]]\nCaptured On: %u,\n#+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n")
+;                               ("L" "Protocol Link" entry (file+headline ,(concat org-directory "Home.org") "Captured Links")
+;                                "* %?[[%:link][%:description]]\nCaptured On: %U")
+;                               ))
 
 ;; Then, add this to ~/.local/share/applications/org-protocol.desktop
 ; [Desktop Entry]
